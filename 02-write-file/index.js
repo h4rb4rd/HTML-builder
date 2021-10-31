f21d5d17ba5820fb0078c8640a11ec71c83ee789
Alexander Danilov <h4rb4rd@gmail.com>
2021-10-31 12:50:03 +0300

    implement: 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,26 @@
+const process = require("process");
+const readline = require("readline");
+const fs = require("fs");
+const path = require("path");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const filePath = path.join(__dirname, "text.txt");
+
+rl.question("Enter your text:\n", (userInput) => {
+  fs.writeFile(filePath, userInput, (error) => {
+    error ? console.log(error) : null;
+  });
+  rl.on("line", (userInput) => {
+    fs.appendFile(filePath, userInput, (error) => {
+      error ? console.log(error) : null;
+    });
+  });
+});
+
+rl.on("close", () => {
+  console.log("Goodbye!");
+});
