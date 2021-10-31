fc330b97afee526a9eb5746b42bf8e697f112bb2
Alexander Danilov <h4rb4rd@gmail.com>
2021-10-31 12:52:35 +0300

    implement: 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const path = require("path");
+
+const src = path.join(__dirname, "css");
+const app = path.join(__dirname, "project-dist");
+const bundle = path.join(app, "bundle.css");
+
+function copyStyles(src, app, bundle) {
+  fs.readdir(src, (err, files) => {
+    err ? console.log(err) : null;
+
+    for (const file of files) {
+      if (path.extname(file) == ".css") {
+        fs.readFile(path.join(src, file), (err, data) => {
+          try {
+            fs.appendFile(bundle, data, (err) => {
+              err ? console.log(err) : null;
+            });
+          } catch {
+            console.log(err);
+          }
+        });
+      }
+    }
+  });
+}
+
+function createbundle(src, app, bundle) {
+  fs.unlink(bundle, (err) => {
+    copyStyles(src, app, bundle);
+  });
+}
+
+createbundle(src, app, bundle);
